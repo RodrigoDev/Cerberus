b6db50496aaa539210f06a1e71a3a8b521664dab
Rodrigo Carneiro <rodrigo.carneiro@olxbr.com>
2016-03-03 20:30:48 -0300

    Adding dummy stub test

--- tests/AclTest.php
+++ tests/AclTest.php
@@ -1,10 +1,24 @@
 <?php
 
-namespace Cerberus\Acl\Contracts;
+namespace Cerberus\Acl\Contracts\Tests;
 
-use Cerberus\Acl;
+use Cerberus\Acl\Acl;
 
 class TestCerberus extends \PHPUnit_Framework_TestCase
 {
+    public function testStub()
+    {
+        // Cria um esboço para a classe Acl.
+        $acl = $this->getMockBuilder('Cerberus\Acl\Acl')
+                    ->disableOriginalConstructor()
+                    ->getMock();
 
+        // Configura o esboço.
+        $acl->method('hasRole')
+             ->willReturn(true);
+
+        // Chamando $esboco->fazAlgumaCoisa() agora vai retornar
+        // 'foo'.
+        $this->assertEquals(true, $acl->hasRole('teste'));
+    }
 }
